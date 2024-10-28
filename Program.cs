33812d75eb79108db6573efa6511194582eb5ce8
MADDIMSETTY Sesha Satya Priyanka (Priyanka) <sesha.satya.priyanka.maddimsetty@hexagon.com>
2024-10-28 12:39:12 +0530

    Updates

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET Core!");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your number squared is: {number * number}");
+        }
+    }
+}
